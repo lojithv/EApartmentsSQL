245daee8287b47b853f397c04204c234b5e9ea32
MalinduSasanga <lkmsasanga@gmail.com>
2023-01-15 14:54:36 +0530

    add users

---
+++ EApartmentsSQL/AddUsers.sql
@@ -0,0 +1,16 @@
+USE [EApartments]
+-- Create a new table called 'Customers' in schema 'dbo'
+-- Drop the table if it already exists
+IF OBJECT_ID('dbo.Users', 'U') IS NOT NULL
+DROP TABLE dbo.Users
+GO
+-- Create the table in the specified schema
+CREATE TABLE dbo.Users
+(
+   CustomerId INT NOT NULL IDENTITY(1,1) PRIMARY KEY, -- primary key column
+   FirstName [NVARCHAR](50) NOT NULL,
+   LastName [NVARCHAR](50) NOT NULL,
+   Email [NVARCHAR](50) NOT NULL,
+   Password [NVARCHAR](50) NOT NULL
+);
+GO
