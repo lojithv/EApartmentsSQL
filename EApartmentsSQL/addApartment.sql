d32b58c27a664fa3452add076c26d9495959fa82
MalinduSasanga <lkmsasanga@gmail.com>
2023-01-15 14:59:44 +0530

    add apartments

---
+++ EApartmentsSQL/addApartment.sql
@@ -0,0 +1,16 @@
+USE [EApartments]
+-- Create a new table called 'Customers' in schema 'dbo'
+-- Drop the table if it already exists
+IF OBJECT_ID('dbo.Apartments', 'U') IS NOT NULL
+DROP TABLE dbo.Apartments
+GO
+-- Create the table in the specified schema
+CREATE TABLE dbo.Apartments
+(
+   ApartmentID INT NOT NULL IDENTITY(1,1) PRIMARY KEY, -- primary key column
+   BuildingID [NVARCHAR](50) NOT NULL,
+   ApartmentType [NVARCHAR](50) NOT NULL,
+   Location [NVARCHAR](50) NOT NULL,
+   Status  [NVARCHAR](50) NOT NULL
+);
+GO
